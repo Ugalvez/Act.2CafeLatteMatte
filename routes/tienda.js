b87ee6061cd84bc4b69b36333864b15f5ec6d868
Sergiooo <scmorillomontes@gmail.com>
2024-10-21 03:37:29 -0500

    Falta Carrito y Mejorar Admin

--- routes/tienda.js
+++ routes/tienda.js
@@ -13,18 +13,38 @@ const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-   console.log("bola",adminData.productos);
+   //console.log("bola",adminData.productos);
 
    //res.sendFile(path.join(raizDir,'views','tienda.html'));
     const productos = adminData.productos;
    res.render('tienda',{
     prods: productos,
-    titulo: "La Tienda",
+    titulo: "Latte&Matte",
     path: "/"
-
-
    })
 
  });
+
+ router.get('/productos', (req, res, next) => {
+  const productos = adminData.productos;
+  res.render('productos',{
+    prods: productos,
+    titulo: "Productos",
+    path: "/productos"
+  })
+ })
+
+ /*
+ router.get('/agregar-productos', (req, res, next) => {
+  res.render('agregar-productos',{
+    titulo: "Agregar Productos",
+    path: "/agregar-productos"
+  })
+ })
+*/
+
+
+
+
 
  module.exports = router;
