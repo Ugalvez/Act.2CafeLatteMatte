0f7f944913a359995f400daa8d2f4386b9d71f83
Ugalvez <ugalvez987@gmail.com>
2024-11-13 16:10:44 -0500

    implementado is-auth, is-admin y express-validator , falta personalizarlo para adaptar mejor a nuestras necesidades

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 
 //const path = require('path');
 const express = require('express');
+const { body } = require('express-validator');
 
 //const raizDir = require('../utils/path');
 
-const adminController = require('../controllers/admin')
+const adminController = require('../controllers/admin');
+const isAdmin = require('../middleware/is-admin');
 
 
 
@@ -13,27 +15,53 @@ const router = express.Router();
 
 
 
-router.get('/crear-producto', adminController.getCrearProducto);
+router.get('/crear-producto',
+   [
+      body('nombre', 'El nombre del producto debe ser una texto de no menos de 5 caracteres')
+          .isString()
+          .isLength({ min: 5 })
+          .trim(),
+      //body('urlImagen').isURL(),
+      body('precio').isFloat(),
+      body('descripcion')
+          .isLength({ min: 10, max: 400 })
+          .trim()
+  ],
+  isAdmin,
+   adminController.getCrearProducto);
 
 
 
 
 
-router.post('/crear-producto', adminController.postCrearProducto);
+router.post('/crear-producto', isAdmin, adminController.postCrearProducto);
 
 
 
 
 
-router.get('/adminHome', adminController.getDisplayProductos);
+router.get('/adminHome', isAdmin, adminController.getDisplayProductos);
 
 
 
-router.get('/editar-producto/:idProducto', adminController.getEditarProducto);
+router.get('/editar-producto/:idProducto', isAdmin, adminController.getEditarProducto);
 
-router.post('/editar-producto', adminController.postEditarProducto);
+router.post('/editar-producto', 
+   [
+      body('nombre', 'El nombre del producto debe ser una texto de no menos de 5 caracteres')
+          .isString()
+          .isLength({ min: 3 })
+          .trim(),
+      //body('urlImagen').isURL(),
+      body('precio').isFloat(),
+      body('descripcion', 'La descripcion debe tener no menos de 10 ni mas de 400 caracteres')
+          .isLength({ min: 10, max: 400 })
+          .trim()
+  ],
+  isAdmin, 
+  adminController.postEditarProducto);
 
-router.post('/eliminar-producto', adminController.postEliminarProducto);
+router.post('/eliminar-producto', isAdmin, adminController.postEliminarProducto);
  
 
    /*
